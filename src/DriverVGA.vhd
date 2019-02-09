0a86247d21f281f6902ca9abda9a980f881ddd25
IldeJS <45567700+IldeJS@users.noreply.github.com>
2019-02-09 12:45:40 +0100

    DriverVGA comentado

--- src/DriverVGA.vhd
+++ src/DriverVGA.vhd
@@ -1,34 +1,25 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:30:26 11/13/2018 
--- Design Name: 
--- Module Name:    DriverVGA - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+-------------------------------------------------------------------
+--	Trabajo Donkey Kong - Complementos de Electrónica	 --
+--	Máster Universitario en Ingeniería de Telecomunicación 	 --
+--	Universidad de Sevilla, Curso 2018/2019			 --	
+--								 --	
+--	Autores:						 --
+--								 --
+--		- José Manuel Gata Romero  			 --
+--		- Ildefonso Jiménez Silva			 --
+--		- Guillermo Palomino Lozano			 --
+--								 --
+-------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+-- Descripción de la entidad: Declaración de puertos del DriverVGA
+-- RGBin: Señal de entrada que indica el color RGB generado por el bloque Control
+-- HS: Señal de salida que indica el pulso de sincronismo horizontal
+-- HV: Señal de salida que indica el pulso de sincronismo vertical
+-- refresh: Señal de salida que indica la terminación de la representación de una pantalla
+-- ejex, ejey: Señales de salida correspondientes con las coordenadas horizontal y vertical, respectivamente
+-- RGBout: Señal de salida que representa el color en 8 bits que se corresponderá con el píxel indicado
 entity DriverVGA is
     Port ( clk : in  STD_LOGIC;
            reset : in  STD_LOGIC;
@@ -40,8 +31,16 @@ entity DriverVGA is
            RGBout : out  STD_LOGIC_VECTOR(7 downto 0));
 end DriverVGA;
 
+-- Descripción de la arquitectura
 architecture Behavioral of DriverVGA is
+	
+-- Declaración de señales internas utilizadas en procesos y para la interconexión de bloques
+signal clk_pixel, pclk_pixel, resets1O, resets2O, Blank_h, Blank_v, enable2 : STD_LOGIC;
+signal Qx, Qy : STD_LOGIC_VECTOR (9 downto 0);
 
+-- Declaración de componentes
+
+-- Contador síncrono de Nbit con habilitación y reset síncrono.
 component contador is
     Generic (Nbit: INTEGER := 8);
 	 Port ( enable : in  STD_LOGIC;
@@ -52,6 +51,7 @@ component contador is
 			  
 end component;
 
+-- Comparador síncrono
 component comparador is
 	Generic (Nbit: integer :=8;
 	End_Of_Screen: integer :=10;
@@ -67,42 +67,9 @@ component comparador is
 			O3 : out STD_LOGIC);
 end component;
 
-signal clk_pixel, pclk_pixel, resets1O, resets2O, Blank_h, Blank_v, enable2 : STD_LOGIC;
-signal Qx, Qy : STD_LOGIC_VECTOR (9 downto 0);
---signal RGB : STD_LOGIC_VECTOR (7 downto 0);
 begin
-
-ejex <= Qx;
-ejey <= Qy;
-refresh <= resets2O;
-
-enable2 <= resets1O AND clk_pixel;
--- Bloque frec_pixel, directamente disenado en el nivel de jerarquia superior
-
-pclk_pixel <= not clk_pixel;
-
-div_frec: process(clk, reset)
-begin
-	if(reset='1') then
-		clk_pixel <= '0';
-	elsif (rising_edge(clk)) then
-		clk_pixel <= pclk_pixel;
-	end if;
-end process;
-
--- Bloque frec_pixel, directamente disenado en el nivel de jerarquia superior
-
-gen_color: process(Blank_H, Blank_V, RGBin)
-begin
-	if (Blank_H ='1' or Blank_V = '1') then
-		RGBout <= (others => '0');
-	else
-		RGBout <= RGBin;
-	end if;
-end process;
-
--- Ahora instanciamos los componentes de jerarquia superior
-			  
+	
+-- Instanciación de cada uno de los componentes ya mencionados arriba.			  
 contadorh: contador
 	GENERIC MAP (Nbit => 10)
 	PORT MAP(
@@ -148,8 +115,36 @@ compv: comparador
 			O1 => Blank_V,
 			O2 => VS,
 			O3 => resets2O);
+	
+	
 
+ejex <= Qx;
+ejey <= Qy;
+refresh <= resets2O;
 
+enable2 <= resets1O AND clk_pixel;
+
+-- Bloque frec_pixel, directamente diseñado en el nivel de jerarquia superior
+pclk_pixel <= not clk_pixel;
+
+-- Proceso contador para dividir la frecuencia de reloj a una frecuencia apreciable por el ojo humano.
+div_frec: process(clk, reset)
+begin
+	if(reset='1') then
+		clk_pixel <= '0';
+	elsif (rising_edge(clk)) then
+		clk_pixel <= pclk_pixel;
+	end if;
+end process;
+
+-- Proceso encargado de no mostrar ningún color cuando se está volviendo a otra línea de la imagen
+gen_color: process(Blank_H, Blank_V, RGBin)
+begin
+	if (Blank_H ='1' or Blank_V = '1') then
+		RGBout <= (others => '0');
+	else
+		RGBout <= RGBin;
+	end if;
+end process;
 		
 end Behavioral;
-
